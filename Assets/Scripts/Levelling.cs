68c0377c15d85dd11c657fa4cfd4670c1a993ee6
SAE1\1027606 <1027606@student.sae.edu.au>
2023-06-05 19:57:30 +1000

    Start to my Porject 1 Dugeon Master.

---
+++ Assets/Scripts/Levelling.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levelling : MonoBehaviour
+{
+    //create a player name
+    //create a lvl
+    //create health
+    //create enemy hp
+    //create a stat pool
+    //create an attack int
+    //create a deffense int
+    //create a luck int
+    //player current exp
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug out current player lvl
+        //debug out player health
+        //player xp
+        //player attack
+        //player defense
+        //player luck
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
