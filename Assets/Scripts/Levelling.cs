57fb9c3e86113f20716d796fbbe8993c36f159b8
TORBJORNGAMINGP\Windows PC <1027606@student.sae.edu.au>
2023-06-06 16:19:42 +1000

    start of pseudocoding, debugging and start of basic attack functions

--- Assets/Scripts/Levelling.cs
+++ Assets/Scripts/Levelling.cs
@@ -5,27 +5,68 @@ using UnityEngine;
 public class Levelling : MonoBehaviour
 {
     //create a player name
-    //create a lvl
+    public string characterName = "Paladin Raphael";
+    //create a current lvl
+    public int currentLevel = 0;
     //create health
-    //create enemy hp
-    //create a stat pool
+    public float currentHealth = 10f;
+    //create Skeleton hp
+    public float skeletonHealth = 10f;
     //create an attack int
+    public int attack = 0;
     //create a deffense int
+    public int defense = 0;
     //create a luck int
+    public int luck = 0;
     //player current exp
+    public float currentPlayerXp = 0;
+    //exp threshold
+    public int xpthreshold = 50;
+    //create a stat pool
+    public int statpool = 15;
 
-
+    //create a Keycode to roll random stat arrangment
+    //public KeyCode rollInputKey; (keep for possible use later
 
     // Start is called before the first frame update
     void Start()
     {
-        //Debug out current player lvl
-        //debug out player health
-        //player xp
-        //player attack
-        //player defense
-        //player luck
 
+        //Debug you have entered the dungeons message
+        Debug.Log("WELCOME TO THE DUGEONS TRAVELER, WHAT AWAITS YOU INSIDE IS EXPERIENCE AND LOOT BEYOND YOUR DREAMS." +
+            " HOWEVER BE WEARY WITH GREAT GREED COMES GREAT DEMISE, " +
+            "THE FURHTER YOU TRAVEL THE DARK YOUR NIGHTMARES WILL BECOME");
+        //Debug out current player lvl
+        Debug.Log("Current lvl: " + currentLevel);
+        //Debug out player health
+        Debug.Log("Health: " + currentHealth);
+        //Debug out player xp
+        Debug.Log("XP: " + currentPlayerXp);
+        //Debug an enemy has spawned (what entity) and its health value.
+        Debug.Log("A clattering sound approachs from the shadows, you face a withered skeleton.");
+        // Debug out skeleton health
+        Debug.Log("Withered skeleton health: " + skeletonHealth);
+
+        int tempstatpool = 15;
+        //calculate random number generated for each stat from statpool
+        attack = Random.Range(0, statpool + 1);
+        statpool -= attack;
+
+        defense = Random.Range(0, statpool);
+        statpool -= defense;
+
+        luck = Random.Range(0, statpool);
+        statpool -= luck;
+
+        Debug.Log("Attack lvl is: " + attack);//Debug out player attack
+        Debug.Log("Defense lvl is: " + defense);//Debug out player defense
+        Debug.Log("Luck lvl is: " + luck);//Debug out player luck
+
+        //create skeleton statpool and assign stats to skeleton
+
+        // add all stat together than / by 3 to workout damage for both player and enemy
+
+        // compare damage from both entities to work out who hits and who misses
 
 
 
@@ -34,6 +75,7 @@ public class Levelling : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+
     }
 }
