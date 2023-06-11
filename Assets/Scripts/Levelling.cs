778c25a1510ac286fda0f44e23ffde18f455fecb
TORBJORNGAMINGP\Windows PC <1027606@student.sae.edu.au>
2023-06-11 23:27:50 +1000

    Adding extra enemy code

--- Assets/Scripts/Levelling.cs
+++ Assets/Scripts/Levelling.cs
@@ -24,6 +24,10 @@ public class Levelling : MonoBehaviour
     public int xpthreshold = 50;
     //create a stat pool
     public int statpool = 15;
+    // Players overall power
+    public int playerPower = 0;
+    // Enemy power
+    public int enemyPower = 0;
 
     //create a Keycode to roll random stat arrangment
     //public KeyCode rollInputKey; (keep for possible use later
@@ -31,7 +35,7 @@ public class Levelling : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        #region text line
         //Debug you have entered the dungeons message
         Debug.Log("WELCOME TO THE DUGEONS TRAVELER, WHAT AWAITS YOU INSIDE IS EXPERIENCE AND LOOT BEYOND YOUR DREAMS." +
             " HOWEVER BE WEARY WITH GREAT GREED COMES GREAT DEMISE, " +
@@ -46,6 +50,7 @@ public class Levelling : MonoBehaviour
         Debug.Log("A clattering sound approachs from the shadows, you face a withered skeleton.");
         // Debug out skeleton health
         Debug.Log("Withered skeleton health: " + skeletonHealth);
+        #endregion
 
         int tempstatpool = 15;
         //calculate random number generated for each stat from statpool
@@ -58,11 +63,29 @@ public class Levelling : MonoBehaviour
         luck = Random.Range(0, statpool);
         statpool -= luck;
 
-        Debug.Log("Attack lvl is: " + attack);//Debug out player attack
-        Debug.Log("Defense lvl is: " + defense);//Debug out player defense
-        Debug.Log("Luck lvl is: " + luck);//Debug out player luck
+        Debug.Log("Your Attack lvl is: " + attack);//Debug out player attack
+        Debug.Log("your Defense lvl is: " + defense);//Debug out player defense
+        Debug.Log("Your Luck lvl is: " + luck);//Debug out player luck
+
+        // Value of player over power
+        playerPower = (attack + defense + luck) / 3;
+
+        Debug.Log("The power within you is: " + playerPower);
+
 
         //create skeleton statpool and assign stats to skeleton
+        int skeletonStats = 15;
+        int skeletonAttack = Random.Range(0, skeletonStats + 1);
+        statpool -= attack;
+
+        int skeletonDefense = Random.Range(0, skeletonStats);
+        statpool -= defense;
+
+        int skeletonLuck = Random.Range(0, skeletonStats);
+        skeletonStats -= luck;
+
+        // Debug Skeleton stats
+        Debug.Log("Skeleton stats are attack:" + skeletonAttack + ", Defense: " + skeletonDefense + ", Luck: " + skeletonLuck);
 
         // add all stat together than / by 3 to workout damage for both player and enemy
 
