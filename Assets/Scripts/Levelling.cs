a997b5faf7f189e82de9aabf2f816f64edc7a8b6
SAE1\1027606 <1027606@student.sae.edu.au>
2023-06-12 17:23:21 +1000

    45min commit, attacking decider coded

--- Assets/Scripts/Levelling.cs
+++ Assets/Scripts/Levelling.cs
@@ -9,9 +9,13 @@ public class Levelling : MonoBehaviour
     //create a current lvl
     public int currentLevel = 0;
     //create health
-    public float currentHealth = 10f;
+    public float currentHealth = 15f;
     //create Skeleton hp
     public float skeletonHealth = 10f;
+    // Levelling for enemy entity
+    public int enemyLevelLow = 0;
+    //Potential for higher levelling enemies
+
     //create an attack int
     public int attack = 0;
     //create a deffense int
@@ -21,20 +25,24 @@ public class Levelling : MonoBehaviour
     //player current exp
     public float currentPlayerXp = 0;
     //exp threshold
-    public int xpthreshold = 50;
+    public float xpthreshold = 50;
     //create a stat pool
     public int statpool = 15;
     // Players overall power
-    public int playerPower = 0;
+    public float playerPower = 0;
     // Enemy power
-    public int enemyPower = 0;
-
+    public float enemyPower = 0;
+    //A value used to multiply the characters attack value on level up
+    public float attackMultiplier = 125.25f;
     //create a Keycode to roll random stat arrangment
+
     //public KeyCode rollInputKey; (keep for possible use later
 
     // Start is called before the first frame update
     void Start()
     {
+        int tempstatpool = 15;
+        
         #region text line
         //Debug you have entered the dungeons message
         Debug.Log("WELCOME TO THE DUGEONS TRAVELER, WHAT AWAITS YOU INSIDE IS EXPERIENCE AND LOOT BEYOND YOUR DREAMS." +
@@ -48,11 +56,14 @@ public class Levelling : MonoBehaviour
         Debug.Log("XP: " + currentPlayerXp);
         //Debug an enemy has spawned (what entity) and its health value.
         Debug.Log("A clattering sound approachs from the shadows, you face a withered skeleton.");
+        //Skelton level
+        enemyLevelLow = Random.Range(0, 6);
+        Debug.Log("Skeleton LvL: " + enemyLevelLow);
         // Debug out skeleton health
         Debug.Log("Withered skeleton health: " + skeletonHealth);
         #endregion
-
-        int tempstatpool = 15;
+        
+        #region Character Stat generator
         //calculate random number generated for each stat from statpool
         attack = Random.Range(0, statpool + 1);
         statpool -= attack;
@@ -63,35 +74,54 @@ public class Levelling : MonoBehaviour
         luck = Random.Range(0, statpool);
         statpool -= luck;
 
-        Debug.Log("Your Attack lvl is: " + attack);//Debug out player attack
-        Debug.Log("your Defense lvl is: " + defense);//Debug out player defense
-        Debug.Log("Your Luck lvl is: " + luck);//Debug out player luck
-
+        Debug.Log("Your Attack lvl is: " + attack + ", your Defense lvl is: " + defense + ", Your Luck lvl is: " + luck); //Debug out players stats
+        
         // Value of player over power
         playerPower = (attack + defense + luck) / 3;
 
         Debug.Log("The power within you is: " + playerPower);
 
+        #endregion
 
+        #region Skeleton Stat Generator
         //create skeleton statpool and assign stats to skeleton
         int skeletonStats = 15;
+
         int skeletonAttack = Random.Range(0, skeletonStats + 1);
-        statpool -= attack;
+        skeletonStats -= skeletonAttack;
 
         int skeletonDefense = Random.Range(0, skeletonStats);
-        statpool -= defense;
+        skeletonStats -= skeletonDefense;
 
         int skeletonLuck = Random.Range(0, skeletonStats);
-        skeletonStats -= luck;
+        skeletonStats -= skeletonLuck;
 
         // Debug Skeleton stats
         Debug.Log("Skeleton stats are attack:" + skeletonAttack + ", Defense: " + skeletonDefense + ", Luck: " + skeletonLuck);
 
         // add all stat together than / by 3 to workout damage for both player and enemy
+        enemyPower = (skeletonAttack + skeletonDefense + skeletonLuck) / 3;
+        Debug.Log("Skeletons power level is: " + enemyPower);
+        #endregion
 
+        #region Deciding attack
         // compare damage from both entities to work out who hits and who misses
+        float deciderPower = 0;
+        deciderPower = (playerPower/enemyPower) * 100;
 
-
+        if (deciderPower > 50)
+        {
+            Debug.Log("You have hit the enemy with a mighty swing, you have damaged the skeleton: " + playerPower + "DMG");
+        }
+        else if (deciderPower < 49)
+        {
+            Debug.Log("You have been slashed by the skeletons decaying hand, you have taken: " + enemyPower + "DMG");
+        }
+        else
+        {
+            Debug.Log("You both missed");
+        }
+        #endregion
 
     }
 
