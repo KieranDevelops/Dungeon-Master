7860211615f23cc0b07e3bc91640e8db0760350b
TORBJORNGAMINGP\Windows PC <1027606@student.sae.edu.au>
2023-06-13 18:04:40 +1000

    Levelling system started and advanced attack system refined

--- Assets/Scripts/Levelling.cs
+++ Assets/Scripts/Levelling.cs
@@ -16,19 +16,21 @@ public class Levelling : MonoBehaviour
     public int defense = 0;//create a deffense int
     public int luck = 0;//create a luck int
     public int currentPlayerXp = 0;//player current exp
-    public int xpthreshold = 50;//exp threshold
+    public int xpThreshold = 0;//exp threshold
     public int statpool = 15;//create a stat pool
     public int playerPowerLevel = 0; // Players overall power 
 
-    public float skeletonHealth = 10f;//create Skeleton hp
+    public float skeletonHealth = 0f;//create Skeleton hp
     public int skeletonAttack = 0;
     public int skeletonDefense = 0;
     public int skeletonLuck = 0;
-    public int enemyLevelLow = 0;// Levelling for enemy entity
+    public int skeletonLevel = 0;// Levelling for enemy entity
     public int enemyPowerLevel = 0;
     //Potential for higher levelling enemies
 
     public float attackMultiplier = 125.25f;//A value used to multiply the characters attack value on level up
+    bool isDead = true;// asking if the enemy or the player is dead
+    int xPGain = 25;
 
     // Start is called before the first frame update
     void Start()
@@ -43,6 +45,28 @@ public class Levelling : MonoBehaviour
         #endregion
         
         #region Character Stat generator
+
+        if(currentLevel == 1)
+        {
+            xpThreshold = 50;
+        }
+        else if(currentLevel == 2)
+        {
+            xpThreshold *= (int)1.5f;
+        }
+        else if(currentLevel == 3)
+        {
+            xpThreshold *= 2;
+        }
+        else if(currentLevel == 4)
+        {
+            xpThreshold *= (int)2.5f;
+        }
+        else
+        {
+            xpThreshold *= 3;
+        }
+
         //calculate random number generated for each stat from statpool
         attack = Random.Range(0, statpool + 1);
         statpool -= attack;
@@ -64,10 +88,25 @@ public class Levelling : MonoBehaviour
 
         //create skeleton statpool and assign stats to skeleton
         // these should consist of attack, strength and luck
-        //Generate a random level for the skeleton between 1-5
+        //Generate a random level for the skeleton between 1-3
+        //Ask what level the skeleton is and calculate the health according to its level
 
-        enemyLevelLow = Random.Range(0, 6);//Skelton level
-        int skeletonStats = 15;
+        skeletonLevel = Random.Range(1, 4);//Skelton level
+        if(skeletonLevel == 1 )
+        {
+            skeletonHealth = skeletonLevel * 10;
+        }
+        else if(skeletonLevel == 2)
+        {
+            skeletonHealth = skeletonLevel * (float)7.5;
+        }
+        else
+        {
+            skeletonHealth = 20;
+        }
+
+
+        int skeletonStats = 10;
 
         skeletonAttack = Random.Range(0, skeletonStats + 1);
         skeletonStats -= skeletonAttack;
@@ -95,25 +134,57 @@ public class Levelling : MonoBehaviour
         int deciderPower = (playerPowerLevel / enemyPowerLevel) * 100;
 
         //Has a skeleton spawned
+        //Ask what the skeletons level is and how much health it has and log it out to the console.
         if(Input.GetKeyDown(KeyCode.Tab))
         {
             Debug.Log("A clattering sound approachs from the shadows, you face a skeleton." + "Press E to attack");//Debug an enemy has spawned (what entity) and its health value.
+            if(skeletonLevel == 1)//We are asking if the skeleton is level 1 and if so log out it is level 1 and has the assigned health value.
+            {
+                Debug.Log("Skeletonlvl: " + "1" + " Health: 10");
+                
+            }
+            else if(skeletonLevel == 2)//We are asking if the skeleton is level 2 and if so log out it is level 2 and has the assigned health value.
+            {
+                Debug.Log("Skeleton lvl: " + "2" + " Health: 15");
+               
+            }
+            else//We are asking if the skeleton is level 3 and if so log out it is level 3 and has the assigned health value.
+            {
+                Debug.Log("Skeletonlvl: " + "3" + " Health: 20");
+            }
         }
          
-        bool isDead = false;
+        //has the skeleton or player died
+        //yes they have
+        //print out a death message and recieve a bonus on enemy death
 
-        if(skeletonHealth <= 0 && !isDead)
+        if(skeletonHealth <= 0 && isDead)
         {
-            isDead = true;
-            Debug.Log("The Skeleton has been defeated, Spawn New Skeleton");
+            skeletonHealth = skeletonLevel;
+            currentPlayerXp += xPGain;
+            Debug.Log("The Skeleton has been defeated, you have recieved 50Xp"); 
+            
             
         }
-        else if(currentHealth <=0 && !isDead)
+        else if(currentHealth <=0 && isDead)
         {
-            isDead = true;
+            currentHealth = 15f;
             Debug.Log("You were not mighty enough restart dungeon");
+            
         }
+        
+        //can player level up? check Xp threshold
+        // yes they can
+        //press L to level up
 
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            if (currentPlayerXp >= xpThreshold)
+            {
+                Debug.Log("You have Leveled up to Lvl: " + currentLevel);
+            }
+
+        }
         
 
         //has the player pressed space bar
@@ -123,9 +194,13 @@ public class Levelling : MonoBehaviour
             //yes they have
             //compare two levels
             //print which player has won, how muchg damage they took
+
             int maxPower = 10;
             playerPowerLevel = Random.Range(playerPowerLevel, maxPower);
             enemyPowerLevel = Random.Range(enemyPowerLevel,maxPower);
+            
+            
+
 
             if (playerPowerLevel > enemyPowerLevel)
             {
